3642176a9f4610eab98f5eba2392a6999f99c2c3
nhuy-git <yb2205922@student.ctu.edu.vn>
2025-11-23 01:01:15 +0700

    Cai dat xu ly loi

--- QuanLyMuonSach-Backend/app.js
+++ QuanLyMuonSach-Backend/app.js
@@ -1,3 +1,4 @@
+const ApiError = require("./api-error");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -24,6 +25,21 @@ app.use("/api/sach", require("./routes/sach"));
 app.use("/api/docgia", require("./routes/docgia"));
 app.use("/api/muontra", require("./routes/muontra"));
 app.use("/api/auth", require("./routes/auth"));
+
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 // Xuất app để server.js sử dụng
 module.exports = app;
